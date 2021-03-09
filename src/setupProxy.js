57447d64f1e022a78a2ff58c107f706470db0cdd
yared1997 <yared@beezsocialerp.com>
2021-03-09 23:39:13 +0300

    added proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,12 @@
+// const { createProxyMiddleware } = require("http-proxy-middleware");
+
+// module.exports = function (app) {
+//   app.use(
+//     "/api",
+//     createProxyMiddleware({
+//       target: "https://api.musixmatch.com/ws/1.1",
+//       changeOrigin: true,
+//       //   secure: false,
+//     })
+//   );
+// };
